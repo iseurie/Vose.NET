9c949fbe03b5357d6451ea54efea4fa28db36515
алый <iseurie@gmail.com>
2018-09-09 05:36:21 -0400

    safety

--- Vose.NET/Library.fs
+++ Vose.NET/Library.fs
@@ -2,23 +2,28 @@ module Vose
 open FSharpPlus
 
 let mutable rnd = new System.Random ()
+
 let mkGen sides dist =
     // scale our probabilities
     let scaled = Array.map ((*) (float sides) >> int) dist
     let p scaled = float scaled / float sides
-    let grps = Seq.groupBy (p >> (<) 1.) scaled
+    let grp s =
+        let strata = Seq.groupBy (p >> (<) 1.) s
+        let small = strata |> Seq.find fst |> snd
+        let large = strata |> Seq.find (fst>>not) |> snd
+        small, large
     let prob = Array.zeroCreate sides
     let alias = Array.zeroCreate sides
-    let small = grps |> Seq.find fst |> snd
-    let large = grps |> Seq.find (fst>>not) |> snd
+    let small, large = grp scaled
     let ps = 
         seq { for l, g in Seq.zip small large do
                 prob.[l] <- scaled.[l]
                 alias.[l] <- g
                 yield (scaled.[g] + scaled.[l] - 1) }
-        |> Seq.groupBy (p >> (<) 1.)
-    let small = ps |> Seq.find fst |> snd
-    let large = ps |> Seq.find (fst>>not) |> snd
+    let small, large =
+        let ls, gs = grp ps
+        Seq.append small ls,
+        Seq.append large gs
     for g in large do prob.[g] <- 1
     for l in small do prob.[l] <- 1
     
