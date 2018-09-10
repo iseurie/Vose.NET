214f258b5afaae80a25d0d14c8353af1d78a8ad9
алый <iseurie@gmail.com>
2018-09-10 13:05:14 -0400

    testbed extensions

--- Vose.NET.Test/Program.fs
+++ Vose.NET.Test/Program.fs
@@ -1,12 +1,16 @@
 ﻿open System.Diagnostics
+open Vose
 
 [<EntryPoint>]
 let main _ =
-    let dist = [| 0.25; 0.25; 0.25; 0.25 |]
-    let dice = Vose.mkGen dist
+    let skewed = [| 0.2; 0.3; 0.5 |]
+    let uniform = [| 0.25; 0.25; 0.25; 0.25 |]
     let timer = Stopwatch.StartNew ()
-    let mu = Seq.initInfinite (ignore>>dice) |> Seq.take 1_000_000 |> Seq.averageBy float
+    let avg dist n =
+        Seq.initInfinite (ignore>>(mkGen dist))
+        |> Seq.take n
+        |> Seq.averageBy (Array.get dist)
+    let mu = avg skewed 10_000_000
     timer.Stop () |> ignore
     printfn "found an average of %f in %fms." mu timer.Elapsed.TotalMilliseconds
     0
-
