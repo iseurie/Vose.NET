f974741f85d1ffa72093d2eb028dc8b323a19e97
алый <iseurie@gmail.com>
2018-09-09 21:59:56 -0400

    bugfix

--- Vose.NET/Library.fs
+++ Vose.NET/Library.fs
@@ -3,32 +3,34 @@ open FSharpPlus
 
 let mutable rnd = new System.Random ()
 
-let mkGen sides dist =
-    // scale our probabilities
-    let scaled = Array.map ((*) (float sides) >> int) dist
-    let p scaled = float scaled / float sides
+let mkGen dist =
+    let sides = Array.length dist
+    let mu = 1. / float sides
     let grp s =
-        let strata = Seq.groupBy (p >> (<) 1.) s
-        let small = strata |> Seq.find fst |> snd
-        let large = strata |> Seq.find (fst>>not) |> snd
+        let strata = Seq.groupBy (snd >> (<) mu) s
+        let unwrap = Option.map (snd>>Seq.map fst) >> Option.defaultValue Seq.empty
+        let small = strata |> Seq.tryFind fst |> unwrap
+        let large = strata |> Seq.tryFind (fst>>not) |> unwrap
         small, large
     let prob = Array.zeroCreate sides
     let alias = Array.zeroCreate sides
-    let small, large = grp scaled
-    let ps = 
-        seq { for l, g in Seq.zip small large do
-                prob.[l] <- scaled.[l]
-                alias.[l] <- g
-                yield (scaled.[g] + scaled.[l] - 1) }
+    let small, large = grp <| Seq.indexed dist
+    let ps = seq {
+        for l, g in Seq.zip small large do
+            prob.[l] <- dist.[l] * float sides 
+            alias.[l] <- g
+            yield g, dist.[l] + dist.[g] - mu }
     let small, large =
         let ls, gs = grp ps
         Seq.append small ls,
         Seq.append large gs
-    for g in large do prob.[g] <- 1
-    for l in small do prob.[l] <- 1
+    for g in large do prob.[g] <- 1.
+    for l in small do prob.[l] <- 1.
     
     let gen () =
         let i = rnd.Next () % sides
-        if p prob.[i] * rnd.NextDouble () > 0.5 then i else alias.[i]
+        if prob.[i] * rnd.NextDouble () > 0.5 then i else alias.[i]
   
     gen
+
+
