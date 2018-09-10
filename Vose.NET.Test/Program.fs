9b01dca351794dbd500d03dcf299a3a25be1a8d8
алый <iseurie@gmail.com>
2018-09-09 23:24:31 -0400

    better testing

--- Vose.NET.Test/Program.fs
+++ Vose.NET.Test/Program.fs
@@ -1,7 +1,12 @@
-﻿[<EntryPoint>]
+﻿open System.Diagnostics
+
+[<EntryPoint>]
 let main _ =
-    let gen = Vose.mkGen [| 0.2; 0.3; 0.5 |]
-    Seq.initInfinite (ignore>>gen) |> Seq.take 30000 |> Seq.averageBy float
-    |> printfn "%A"
+    let dist = [| 0.25; 0.25; 0.25; 0.25 |]
+    let dice = Vose.mkGen dist
+    let timer = Stopwatch.StartNew ()
+    let mu = Seq.initInfinite (ignore>>dice) |> Seq.take 1_000_000 |> Seq.averageBy float
+    timer.Stop () |> ignore
+    printfn "found an average of %f in %fms." mu timer.Elapsed.TotalMilliseconds
     0
 
