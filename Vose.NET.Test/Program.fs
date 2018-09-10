55292623a44498d70bf4f8f09df686ffeda67184
алый <iseurie@gmail.com>
2018-09-09 22:00:52 -0400

    bugfix

---
+++ Vose.NET.Test/Program.fs
@@ -0,0 +1,7 @@
+﻿[<EntryPoint>]
+let main _ =
+    let gen = Vose.mkGen [| 0.2; 0.3; 0.5 |]
+    Seq.initInfinite (ignore>>gen) |> Seq.take 30000 |> Seq.averageBy float
+    |> printfn "%A"
+    0
+
